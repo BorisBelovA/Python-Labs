787782cca8dc60c9c9c733ec59a8cf621a651e46
BorisBelovA <belov.boris.a@gmail.com>
2018-09-08 21:03:41 +0300

    Added decrypt_caesar and encrypt_caesar with shift

--- caesar.py
+++ caesar.py
@@ -1,33 +1,33 @@
-def encrypt_caesar(plaintext):
+def encrypt_caesar(plaintext, shift):
     ciphertext = ''
     for char in plaintext:
         if 65 <= ord(char) <= 90:
             if 88 <= ord(char) <= 90:
-                ciphertext+=chr(ord(char) - 23)
+                ciphertext+=chr(ord(char) - 26 + shift)
             else:
-                ciphertext += chr(ord(char) +3)
+                ciphertext += chr(ord(char) + shift)
         elif 97 <= ord(char) <= 122:
             if 120 <= ord(char) <= 122:
-                ciphertext+=chr(ord(char) - 23)
+                ciphertext+=chr(ord(char) - 26 + shift)
             else:
-                ciphertext += chr(ord(char) +3)
+                ciphertext += chr(ord(char) + shift)
         else:
             ciphertext += char
     return ciphertext
 
-def decrypt_caesar(ciphertext):
+def decrypt_caesar(ciphertext, shift):
     plaintext = ''
     for char in ciphertext:
         if 65 <= ord(char) <= 90:
             if 65 <= ord(char) <= 67:
-                plaintext+=chr(ord(char) + 23)
+                plaintext+=chr(ord(char) + 26 - shift)
             else:
-                plaintext += chr(ord(char) - 3)
+                plaintext += chr(ord(char) - shift)
         elif 97 <= ord(char) <= 122:
             if 97 <= ord(char) <= 99:
-                plaintext += chr(ord(char) + 23)
+                plaintext+=chr(ord(char) + 26 - shift)
             else:
-                plaintext += chr(ord(char) - 3)
+                plaintext += chr(ord(char) - shift)
         else:
             plaintext += char
     return plaintext
@@ -35,5 +35,5 @@ def decrypt_caesar(ciphertext):
 
 plaintext = 'python'
 
-print(encrypt_caesar('Python3.2'))
-print(decrypt_caesar('Sbwkrq3.2'))
+print(encrypt_caesar('Python3.2', 3))
+print(decrypt_caesar('Sbwkrq3.2',3))
