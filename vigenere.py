ce31ddcec3779ac9c40f2967b5105896d88ca8d1
BorisBelovA <belov.boris.a@gmail.com>
2018-09-09 10:44:37 +0300

    Completed Vigenere

--- vigenere.py
+++ vigenere.py
@@ -13,28 +13,46 @@ def encrypt_vigenere(plaintext, keyword):
     keyword = makeKey(plaintext, keyword)
     for i in range(len(plaintext)):
         shift = dict[keyword[i]]
-        if(shift+ord(plaintext[i]) > 90):
-            ciphertext += chr((shift + ord(plaintext[i])) - 90 + 65 - 1)
+        if(65 <= ord(plaintext[i]) <= 90):
+            if(shift+ord(plaintext[i]) > 90):
+                ciphertext += chr((shift + ord(plaintext[i])) - 26)
+            else:
+                ciphertext += chr(shift + ord(plaintext[i]))
+        elif (97 <= ord(plaintext[i]) <= 122):
+            if (shift + ord(plaintext[i]) > 122):
+                ciphertext += chr((shift + ord(plaintext[i])) - 26)
+            else:
+                ciphertext += chr(shift + ord(plaintext[i]))
         else:
-            ciphertext += chr(shift+ord(plaintext[i]))
-        print(shift + ord(plaintext[i]))
+            ciphertext += plaintext[i]
+        #print(shift + ord(plaintext[i]))
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext, keyword):
-    """
-    decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    decrypt_vigenere("python", "a")
-    'python'
-    decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
-    # PUT YOUR CODE HERE
     plaintext = ''
+    keyword = makeKey(ciphertext, keyword)
+    for i in range(len(ciphertext)):
+        shift = dict[keyword[i]]
+        if (65 <= ord(ciphertext[i]) <= 90):
+            if((ord(ciphertext[i]) - shift) < 65):
+                plaintext += chr((ord(ciphertext[i]) - shift) + 26)
+            else:
+                plaintext += chr(ord(ciphertext[i]) - shift)
+        elif (97 <= ord(ciphertext[i]) <= 122):
+            if ((ord(ciphertext[i]) - shift) < 97):
+                plaintext += chr((ord(ciphertext[i]) - shift) + 26)
+            else:
+                plaintext += chr(ord(ciphertext[i]) - shift)
+        else:
+            plaintext+=ciphertext[i]
     return plaintext
 
-
-print(encrypt_vigenere('ATTACKATDAWN', "A"))
-
+plaintext = 'ATTACKATDAWN'
+#plaintext = 'attackatdawn'
+key = 'LEMON'
+ciphertext = encrypt_vigenere(plaintext, key)
+print('Шифр: ', ciphertext)
+plaintext = decrypt_vigenere(ciphertext, key)
+print('Изначальное: ', plaintext)
 
